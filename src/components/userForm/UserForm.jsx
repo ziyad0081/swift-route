095f7c18cf8f582ea3d196a742d93f5a5177aebd
Ziyad O. Chaalel <omar.ziyad.chaalel@ensia.edu.dz>
2024-04-25 19:07:28 +0100

    Added hospital services options

--- src/components/userForm/UserForm.jsx
+++ src/components/userForm/UserForm.jsx
@@ -8,7 +8,8 @@ import services from '../../assets/data/hospitals.json'
 function UserForm(props){
     const [selectedValue, setSelected] = useState('')
     const options = Object.keys(services.services).sort()
-    const select = <select className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={selectedValue} onChange={(e)=>{setSelected(e.target.value)}}>
+    const select = <select className="shadow appearance-none border rounded transition-all ease-linear hover:ring-4 hover:ring-sky-500 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value={selectedValue} onChange={(e)=>{setSelected(e.target.value)}}>
+        <option disabled selected value="">Choose a service</option>
     {options.map((option) => (
       <option key={option} value={option}>
         {option}
@@ -19,7 +20,7 @@ function UserForm(props){
     const [userLng, setUserLng] = props.longitude
     const [userAddress, setAddress] = useState("")
     const flyTo = props.flyTo
-    const [userReqService, setUserRequestedService] = useState('General')
+    const [userReqService, setUserRequestedService] = useState(null)
     
     async function handleGeocoderResult(){
         const api_response = await axios.get('https://nominatim.openstreetmap.org/search?q='+ userAddress +'&format=json')
@@ -59,10 +60,10 @@ function UserForm(props){
     return (
     <div className='flex  logo-font flex-col items-center h-full p-4 w-1/2'>
         <h1 className="font-bold text-center text-sky-600 text-3xl w-80"> Enter your address and requested service below and hit go !</h1>
-        <p className="mt-10 mb-2">Alternatively you can drop a marker on the map to locate your current position</p>
+        <p className="mt-10 mb-2">Alternatively you can <span className="font-bold text-sky-500">drop a marker</span> on the map to locate your current position</p>
         <hr className="w-full h-px bg-gray-200 border-none  mb-10"/>
         <label htmlFor="adress" className=" self-start ml-4 mb-4 font-bold text-xl"> Adress </label>
-        <input type="text" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="adress" value={userAddress} onChange={(e)=>setAddress(e.target.value)} placeholder="Jane Ave, John City, Jeffrson Country..."/>
+        <input type="text" className="shadow appearance-none border rounded transition-all ease-linear hover:ring-4 hover:ring-sky-500 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="adress" value={userAddress} onChange={(e)=>setAddress(e.target.value)} placeholder="Jane Ave, John City, Jeffrson Country..."/>
         <hr className="w-full h-px bg-gray-200 border-none mt-4 mb-4"/>
         <label htmlFor="adress" className=" self-start ml-4 mb-4 font-bold text-xl"> Emergency Service </label>
         {select}
