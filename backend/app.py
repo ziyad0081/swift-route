39a1eb94499f9e767faa7b4e31e1ad99fbcd432b
Ziyad O. Chaalel <omar.ziyad.chaalel@ensia.edu.dz>
2024-05-03 10:28:13 +0100

    Backend endpoint function adapted to return all necessary info

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,8 @@ import osmnx as ox
 import matplotlib.pyplot as plt # type: ignore
 import numpy as np
 import os
+from HCSolver import LocalSearchSolver
+from HealthcareNetworkProblem import HealthcareNetworkProblem
 FILE_PATH = "algiers.graphml"
 if os.path.exists(FILE_PATH):
     G = ox.load_graphml(FILE_PATH)
@@ -22,8 +24,13 @@ with open(os.path.join(os.getcwd(),'src','assets','data','hospitals.json')) as f
 with open(os.path.join(os.getcwd(),'src','assets','data','hospital_info.json')) as g:
     hospitals = json.load(g)
 
-@app.route("/get_nearest", methods=['POST'])
-def ReturnNearestHospital():
+P = HealthcareNetworkProblem(None,None,G)
+
+hc = LocalSearchSolver(P)
+
+@app.route("/debug", methods=['POST'])
+def DebugingEndPoint():
+    #USED FOR DEBUGGING , WILL BE REMOVED LATER.
     req_data = request.get_json()["data"]
     user_lat,user_lng, user_serv  = req_data["lat"],req_data["lng"],req_data["serv"]
     possible_hospitals_by_service = [hospital['hospital_id'] for hospital in services[user_serv]]
@@ -32,7 +39,48 @@ def ReturnNearestHospital():
     close_lat , close_lng = closest_hospital["lat"], closest_hospital["lng"]
     hospital_node = ox.nearest_nodes(G,close_lng, close_lat)
     user_node = ox.nearest_nodes(G, user_lng, user_lat)
-    route = ox.shortest_path(G,user_node,hospital_node)
+    max_nodes = len(list(G.nodes()))
+    P.reinitialize_states(user_node, hospital_node)
+    
+    route = hc.SimulatedAnnealingWrapper()
+    return json.dumps(route)
+
+
+@app.route("/get_nearest", methods=['POST'])
+def ReturnNearestHospital():
+    #recieve request body
+    req_data = request.get_json()["data"]
+    #get user coordinates and requested service
+    user_lat,user_lng, user_serv  = req_data["lat"],req_data["lng"],req_data["serv"]
+
+    #get all fitting candidate hospital ids (ones providing user_serv)
+    possible_hospitals_by_service = [hospital['hospital_id'] for hospital in services[user_serv]]
+
+    #get hospital names via id
+    possible_hospitals_by_name = [hospitals[i] for i in possible_hospitals_by_service]
+
+    #estimate closest of candidates by heuristic
+    closest_hospital = min(possible_hospitals_by_name, key=lambda hospital: distance.great_circle(user_lat, user_lng, hospital["lat"], hospital["lng"]))
+
+    #retrieved closest hospital coordinates
+    close_lat , close_lng = closest_hospital["lat"], closest_hospital["lng"]
+
+    #initiate goal and initial nodes
+    hospital_node = ox.nearest_nodes(G,close_lng, close_lat) #goal_node
+    user_node = ox.nearest_nodes(G, user_lng, user_lat) #initial_node
+    max_nodes = len(list(G.nodes())) #nodes count in the graph (used in search statistics)
+
+    P.reinitialize_states(user_node, hospital_node) #initialize problem with our states
+    
+    route_info = hc.KLocalBeamSearchWrapper(5) #This call here is the only thing that will differ between search algorithms
+
+    route = route_info.get("path") #get the path from returned dict
+
+    route_info.pop("path") #removed it to return the rest without redundancy
+
+    route_info["max_nodes"] = max_nodes  #add the graph nodes count from before
+
+    #This section processes route graph nodes and constructs all possible road geometries for edges (so as not to make all roads a straight line) 
     route_json = [{'lat': G.nodes[node]['y'], 'lng': G.nodes[node]['x']} for node in [route[0]]]
     for i in range(len(route)-1):
         if 'geometry' in G[route[i]][route[i+1]][0]:
@@ -41,11 +89,14 @@ def ReturnNearestHospital():
             for point in edge_geo:
                 route_json.append({'lat': point[1], 'lng': point[0]})
             route_json.append({'lat': G.nodes[route[i+1]]['y'], 'lng': G.nodes[route[i+1]]['x']})
-    # route_json = [{'lat': G.nodes[node]['y'], 'lng': G.nodes[node]['x']} for node in route]
+    
+    #this object holds all information to be returned to the frontend 
     returned_info = {"hos_name" : closest_hospital["name"], 
-                     "iter" : route_json}
+                     "iter" : route_json,
+                     "details" : route_info}
+    #return json to frontend
     return json.dumps(returned_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
+
